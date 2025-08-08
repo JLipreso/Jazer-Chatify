4230aed6830d36b1403102d8b83f88c9c68ae5fa
JLipreso <lipreso.developer@gmail.com>
2025-08-08 23:11:41 +0800

    Structure updated

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'api'], function () {
     Route::group(['prefix' => 'chatify'], function () {
-        Route::get('test', function () {
-            echo "OK";
+
+        Route::group(['prefix' => 'message'], function () {
+            Route::get('send', [Jazer\Chatify\Http\Controllers\Message\Send::class, 'send']);
+        });
+
+        Route::group(['prefix' => 'attachment'], function () {
+
+        });
+
+        Route::group(['prefix' => 'convo'], function () {
+
         });
     });
 });
---
+++ src/Http/Controllers/Message/Send.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Jazer\Chatify\Http\Controllers\Message;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+
+class Send extends Controller
+{
+    public static function send(Request $request) {
+        return [];
+    }
+}
