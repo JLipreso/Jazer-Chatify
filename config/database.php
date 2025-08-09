b499466dc99f0fe31372dd5e28d19d3163313602
JLipreso <lipreso.developer@gmail.com>
2025-08-10 02:08:41 +0800

    OK

--- config/database.php
+++ config/database.php
@@ -3,11 +3,11 @@
 return [
     'database_connection' => [
         'driver'        => 'mysql',
-        'host'          => env('CONN_CHATIFY_IP', config('chatifyconfig.conn_chatify_ip')),
-        'port'          => env('CONN_CHATIFY_PT', config('chatifyconfig.conn_chatify_pt')),
-        'database'      => env('CONN_CHATIFY_DB', config('chatifyconfig.conn_chatify_db')),
-        'username'      => env('CONN_CHATIFY_UN', config('chatifyconfig.conn_chatify_un')),
-        'password'      => env('CONN_CHATIFY_PW', config('chatifyconfig.conn_chatify_pw')),
+        'host'          => env('CONN_CHATIFY_IP', config('jtchatifyconfig.conn_chatify_ip')),
+        'port'          => env('CONN_CHATIFY_PT', config('jtchatifyconfig.conn_chatify_pt')),
+        'database'      => env('CONN_CHATIFY_DB', config('jtchatifyconfig.conn_chatify_db')),
+        'username'      => env('CONN_CHATIFY_UN', config('jtchatifyconfig.conn_chatify_un')),
+        'password'      => env('CONN_CHATIFY_PW', config('jtchatifyconfig.conn_chatify_pw')),
         'charset'       => 'utf8mb4',
         'collation'     => 'utf8mb4_unicode_ci',
         'prefix'        => ''
